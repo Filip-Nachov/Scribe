aaa8ec246054d7badc39890d42f64aa93bb4c5ec
Filip Nachov <fnachov@gmail.com>
2024-07-09 16:42:47 +0200

    made it able to moce

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 Scribe : scribe.c
-	$(CC) scribe.c -o scribe -Wall -Wextra -pedantic -std=c99
+	$(CC) scribe.c -o scribe
--- scribe.c
+++ scribe.c
@@ -1,5 +1,33 @@
+// Includes
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+// making a structure
+struct termios origin;
+
+// disable raw mode at end
+void DisableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &origin);
+}
+
+// enable raw mode (disabling echo)
+void EnableRawMode() {
+    tcgetattr(STDIN_FILENO, &origin);
+    atexit(DisableRawMode); // disable raw mode at end
+
+    struct termios raw = origin;
+    
+    raw.c_lflag &= ~(ECHO); // disable echo 
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw);
+}
 
 int main() {
-    return 0;
+      EnableRawMode();
+
+      char c;
+      while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+      return 0;
 }
