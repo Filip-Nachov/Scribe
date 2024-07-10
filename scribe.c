5eeff1cc866f2f2ee1044128cd4897b88ab14df4
Filip Nachov <fnachov@gmail.com>
2024-07-11 01:19:05 +0200

    turned off more stuff

--- scribe.c
+++ scribe.c
@@ -21,7 +21,9 @@ void EnableRawMode() {
     struct termios raw = origin;
    
     // disableing stuff
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN| ISIG); 
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw);
