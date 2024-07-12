d06e839d9e55b757730e493cc2e846be87509826
Filip Nachov <fnachov@gmail.com>
2024-07-12 17:32:12 +0200

    added the 'EditorDrawRows' func to the 'EditorRefreshScreen' oh and fixed anotha typo (they aint escaping me)

--- scribe.c
+++ scribe.c
@@ -70,7 +70,7 @@ char EditorReadKey() {
 void EditorDrawRows() {
     int y;
     for (y = 0; y < 24; y++) {
-        write(STDOUT_FILENO, "~\r\n");
+        write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
 
@@ -79,6 +79,8 @@ void EditorRefreshScreen() {
     write(STDOUT_FILENO,  "\x1b[H", 3);
 
     EditorDrawRows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** input ***/
