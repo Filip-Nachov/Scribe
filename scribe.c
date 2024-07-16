585ed1f8e65b08cba2f372dacbdf170b5e422ce4
Filip Nachov <fnachov@gmail.com>
2024-07-17 00:46:30 +0200

    hiden cursor while repainting

--- scribe.c
+++ scribe.c
@@ -144,12 +144,14 @@ void EditorDrawRows(struct abuf *ab) {
 void EditorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab,  "\x1b[H", 3);
 
     EditorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
